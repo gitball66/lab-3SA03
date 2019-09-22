8faa27fcb6db4ef5f9b18901838bb7c3d7462655
SIVAKORN INGKAWATCHARAKUL <sivakorn_ing@hotmail.co.th>
2019-09-22 23:59:15 +0700

    9

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
             <WordCard value={this.state.value}/>
             <h1>Hint</h1>
             <p>{this.state.hint}</p>
-            <p><br></br>create by sivakorn 5935512015</p>
+            <p><br></br>Create By Sivakorn 5935512015</p>
           </center>      
       </div>
     );
