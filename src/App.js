5687d202d7d89a2e518e58e9449d8cebca5ff9ba
SIVAKORN INGKAWATCHARAKUL <sivakorn_ing@hotmail.co.th>
2019-09-22 21:54:50 +0700

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import WordCard from './WordCard';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: '',hint: ''};
+  }
+  popup = () => {
+    var xans,xhint;
+    var answer = prompt("Player 1 : Please enter your some text:", "EX.HELLO");
+    var hint = prompt("Player 1 : Please enter your hint:", "Ex.say every day");
+    if (answer == null || answer == "") {
+      xans = "";
+    } else {
+      xans = answer;
+    }
+    if (hint == null || hint == "") {
+      xhint = "No have hint!!";
+  } else {
+      xhint = hint;
+  }
+    this.state.value = xans;
+    this.state.hint = xhint;
+  }
+
+  render() {
+    return ( 
+      <div>
+          {this.popup()}   
+          <center> 
+            <h1>Player 2</h1>
+            <WordCard value={this.state.value}/>
+            <h1>Hint</h1>
+            <p>{this.state.hint}</p>
+            <p><br></br>create by airx</p>
+          </center>      
+      </div>
+    );
+  }
 }
 
 export default App;
